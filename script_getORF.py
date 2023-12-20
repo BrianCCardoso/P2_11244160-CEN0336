cf0732b6eadbc15033fc1ba3094cda76d28b7831
BrianCCardoso <brianccardoso@usp.br>
2023-12-20 18:02:46 -0300

    Uptdate

--- script_getORF.py
+++ script_getORF.py
@@ -9,27 +9,30 @@ dic_genes = {}
 class NotFASTAError(Exception):
     pass
 
+#função que retorna as fases de leituras 
 def open_reading_frame(dic_genes):
-    dic_genes_openrc = {}
-    for id in dic_genes.keys():
-        dic_genes_openrc[id] = {'1': '','2' : '','3' : '','4' : '','5' : '','6' : ''}
+    dic_genes_openrc = {} #criação da variável
+    for id in dic_genes.keys(): #loop para acessar o conteudo do dicionario
+        dic_genes_openrc[id] = {'1': '','2' : '','3' : '','4' : '','5' : '','6' : ''}#criacao do dicionario com as 6 fases de leitura
         tratando_sequencia = dic_genes[id]
-        tratando_sequencia_reverso = tratando_sequencia[::-1].replace('G','c').replace('C','g').replace('T','a').replace('A','t').upper()
-        for i in range(0,3):
+        tratando_sequencia_reverso = tratando_sequencia[::-1].replace('G','c').replace('C','g').replace('T','a').replace('A','t').upper()#4,5,6 fase de leitura
+        for i in range(0,3):#atribuindo os valores no dicionario
             dic_genes_openrc[id][str(i+1)] = tratando_sequencia[i:]
             dic_genes_openrc[id][str(i+4)] = tratando_sequencia_reverso[i:]
             i+=1
     return dic_genes_openrc
 
+#funcao responsavel por retornar o max ORF
 def max_reading_frame(dic_openrc):
-    dic_max_ORF = {}
-    for id in dic_openrc.keys():
-        dic_max_ORF[id] = {'Sequência' : '', 'Começo' : '', 'Fim' : '', 'Frame de leitura' : '', 'Tamanho' : 0}
+    dic_max_ORF = {}#criacao do dicioario
+    for id in dic_openrc.keys():#acessando os valores do dicionario
+        dic_max_ORF[id] = {'Sequência' : '', 'Começo' : '', 'Fim' : '', 'Frame de leitura' : '', 'Tamanho' : 0}#criacao do dicionario nesse formato para futuro uso das informacoes
         for frame in dic_openrc[id]:
             sequencia_compilada = ''.join(dic_openrc[id][frame])
-            for match in re.finditer(r'ATG.*?(TAA|TGA|TAG)',sequencia_compilada):
+            for match in re.finditer(r'ATG.*?(TAA|TGA|TAG)',sequencia_compilada):#buscando todas as opcoes de orf de acordo com a expressao regular
                 orf = match.group()
-                if len(orf) > len(dic_max_ORF[id]['Sequência']) and len(orf) % 3 == 0:
+                if len(orf) > len(dic_max_ORF[id]['Sequência']) and len(orf) % 3 == 0:#busca a maior orf caso seja divisivel por 3
+                    #atribuicao dos valores no dicionario
                     dic_max_ORF[id]['Sequência'] = orf
                     dic_max_ORF[id]['Começo'] = match.start() + 1
                     dic_max_ORF[id]['Fim'] = match.end() +1
@@ -38,6 +41,7 @@ def max_reading_frame(dic_openrc):
     return dic_max_ORF
 
 def tranlation(dic_max):
+    #criacao de um dicionario com as informacoes dos codons
     translation_table = {
     'GCT':'A', 'GCC':'A', 'GCA':'A', 'GCG':'A',
     'CGT':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R', 'AGA':'R', 'AGG':'R',
@@ -61,38 +65,40 @@ def tranlation(dic_max):
     'GTT':'V', 'GTC':'V', 'GTA':'V', 'GTG':'V',
     'TAA':'*', 'TGA':'*', 'TAG':'*'
 }
-    dic_proteina = {}
+    dic_proteina = {}#criacao do dicionario de proteinas
     for id in dic_max.keys():
         proteina = ''
         seq_orf = dic_max[id]['Sequência']
-        codons = re.findall(r'(.{3})',seq_orf)
+        codons = re.findall(r'(.{3})',seq_orf)#divisao em codons das ORFS
         for codon in codons:
             if codon in translation_table:
-                proteina += translation_table[codon]
+                proteina += translation_table[codon]#atribuicao dos valores para a variavel proteina
             else:
-                print(f'Esse codon {codon} não existe')
+                print(f'Esse codon {codon} não existe')#caso o codon nao seja codificado
                 sys.exit()
-        dic_proteina[id] = proteina
+        dic_proteina[id] = proteina#atribuicao dos valores no dicionario
     return dic_proteina
 
 
 try:
-    arquivo = 'C:\\Users\\bccle\\OneDrive\\Documentos\\P2_11244160-CEN0336\\arquivo_fasta.fa'
-    if not arquivo.endswith('.fa'):
+    arquivo = sys.argv[1]#recebendo o local do arquivo com o modulo sys
+    if not arquivo.endswith('.fa'):#verificacao se é do tipo fasta
         raise NotFASTAError('Não é um arquivo fasta')
-    with open (arquivo,'r') as arquivo_lido:
+    with open (arquivo,'r') as arquivo_lido:#abrindo o arquivo para leitura
         for linha in arquivo_lido:
-            if linha.startswith('>'):
+            if linha.startswith('>'):#identificando a headline
                 linha = linha.rstrip()
                 gene_id = re.search(r'>(\S+)(\s+)?',linha)
                 gene_id = gene_id.group(1)
                 dic_genes[gene_id] = ''
                 sequencia = ''
             else:
-                sequencia += linha.rstrip().upper()
-                dic_genes[gene_id] = sequencia
+                sequencia += linha.rstrip().upper()#identificando a sequencia
+                dic_genes[gene_id] = sequencia#atribuindo a sequencia ao seu id
+        #dicionarios principais do programa
         dic_max_orf = max_reading_frame(open_reading_frame(dic_genes))
         dic_proteinas = tranlation(dic_max_orf)
+        #fazendo a parte de escrita nos arquivos de saida
         with open('ORF.fna','w') as saida_fna:
             for id in dic_max_orf.keys():
                 saida_fna.write(f">{id}_frame_{dic_max_orf[id]['Frame']}_{dic_max_orf[id]['Começo']}_{dic_max_orf[id]['Fim']}\n")
@@ -103,8 +109,6 @@ try:
                 saida_faa.write(f">{id}_frame_{dic_max_orf[id]['Frame']}_{dic_max_orf[id]['Começo']}_{dic_max_orf[id]['Fim']}\n")
                 for i in range(0, len(dic_proteinas[id]),60):
                     saida_faa.write(dic_proteinas[id][i:i+60]+'\n')
-
-
 except IndexError:
     print('Por favor insira o nome de um arquivo')
 except IOError:
